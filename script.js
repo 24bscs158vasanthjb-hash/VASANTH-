d8d5a19e93699a433fe9088761ada4af7fd02814
24bscs158vasanthjb-hash <24bscs158vasanthjb@skacas.ac.in>
2025-09-16 16:27:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contactForm').onsubmit = function(e) {
-  e.preventDefault();
-  document.getElementById('formMessage').textContent = 
-    'Thank you for contacting! Your message has been received.';
-  document.getElementById('contactForm').reset();
-};
